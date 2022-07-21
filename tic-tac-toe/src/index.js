112dbf627bbd102496f3f8f9ce99b921d4d899ba
Andrew Aramouni <aramouni8@gmail.com>
2022-07-21 07:31:23 -0400

    removed some files etc.

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -1,11 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+// import ReactDOM from "react-dom/client";
+import { render } from "react-dom";
 import App from "./App";
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
-);
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
