8d8abc443252eb8c1e5443870021f947d44817ff
Andrew Aramouni <aramouni8@gmail.com>
2022-07-24 14:03:51 -0400

    make the line overlap eachover and have a tictactoe grid

--- tic-tac-toe/src/TicTocGrid.js
+++ tic-tac-toe/src/TicTocGrid.js
@@ -3,11 +3,26 @@
 export default function TicTocGrid() {
   return (
     <>
-      <div className="container">
+      {/* <div className="container">
         <div className="leftVerticalBar"></div>
         <div className="rightVericalBar"></div>
         <div className="topHorizontalBar"></div>
         <div className="bottomHorizontalBar"></div>
+      </div> */}
+      <div className="main">
+        {/* <div class="maincolumn">
+          <div class="container">
+            <h2>****this is going to be the scoreboard******</h2>
+          </div>
+        </div> */}
+        <div className="maincolumn">
+          <div>
+            <div className="leftVerticalBar"></div>
+            <div className="rightVerticalBar"></div>
+            <div className="topHorizontalBar"></div>
+            <div className="bottomHorizontalBar"></div>
+          </div>
+        </div>
       </div>
     </>
   );
