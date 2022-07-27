e354d4315b4ff5f3405b94f83590525043399fa9
Andrew Aramouni <aramouni8@gmail.com>
2022-07-26 20:20:36 -0400

    delete some comments etc. cleans some code

--- tic-tac-toe/src/TicTocGrid.js
+++ tic-tac-toe/src/TicTocGrid.js
@@ -6,31 +6,55 @@ export default function TicTocGrid() {
   //const player2 = o
 
   //  \/\/\/\/\/\/\/\/\/
-
+  //  NEXT TIME:
+  //     FIGURE OUT A WAY TO PUT THE X FIRST THEN O(BACK AND FORTH). ON EACH SPACE PER TURN
+  //     OR PER CLICK. THE TOGGLING WORKED BUT THE MAYBE MAKE IT A LITTLE BIT MORE SIMPLIER MIND SET
+  //     NEXT TIME. LIKE, MAKE SURE IT GOES TO THE CORRECT SPACES FIRST. THEN
   // const [x, setX] = useState("X");
   // const [o, setO] = useState("O");
+  const [clickedSpace, setClickedSpace] = useState("");
+  const [blank, setBlank] = useState("");
 
-  // const player1 = x;
-  // const player2 = o;
+  const player1 = "X";
+  const player2 = "O";
 
-  // function handleClickXorO() {}
+  //if last clicked value was "X" then next clicked space is "O"
+  function handleClickXorO() {
+    if ("X" === clickedSpace) {
+      setClickedSpace(player2);
+      setBlank("O");
+    } else if ("O" === clickedSpace) {
+      setClickedSpace(player1);
+      setBlank("X");
+    } else {
+      setClickedSpace(player1);
+      setBlank("X");
+    }
+    // return <div className="space">{clickedSpace}</div>;
+  }
 
   return (
     <>
       <div className="topRow">
-        <div className="topLeft space"></div>
-        <div className="topCenter space"></div>
-        <div className="topRight space"></div>
+        <div className="topLeft space" onClick={handleClickXorO}>
+          {clickedSpace === "X" ? player2 : ""}
+        </div>
+        <div className="topCenter space" onClick={handleClickXorO}>
+          {clickedSpace === "X" ? player2 : player1}
+        </div>
+        <div className="topRight space" onClick={handleClickXorO}>
+          {clickedSpace === "X" ? player2 : player1}
+        </div>
       </div>
       <div className="middleRow">
-        <div className="middleLeft space"></div>
-        <div className="middleCenter space"></div>
-        <div className="middleRight space"></div>
+        <div className="middleLeft space" onClick={handleClickXorO}></div>
+        <div className="middleCenter space" onClick={handleClickXorO}></div>
+        <div className="middleRight space" onClick={handleClickXorO}></div>
       </div>
       <div className="bottomRow">
-        <div className="bottomLeft space"></div>
-        <div className="bottomMiddle space"></div>
-        <div className="bottomRight space"></div>
+        <div className="bottomLeft space" onClick={handleClickXorO}></div>
+        <div className="bottomMiddle space" onClick={handleClickXorO}></div>
+        <div className="bottomRight space" onClick={handleClickXorO}></div>
       </div>
     </>
   );
