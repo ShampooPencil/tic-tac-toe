7522b0775db3a69c06a46bffb36372994128c0ab
Andrew Aramouni <aramouni8@gmail.com>
2022-09-22 09:12:03 -0400

    the logic is working as in X and O every other

--- tic-tac-toe/src/Game.js
+++ tic-tac-toe/src/Game.js
@@ -1,11 +1,1 @@
-// import { useState } from "react";
-// import TicTocGrid from "./TicTocGrid.js";
-
-// export default function Game() {
-//   // const [x, setX] = useState("X");
-//   // const [o, setO] = useState("O");
-
-//   function placeX() {
-//     const clickForX = "X";
-//   }
-// }
+export default function Game(props) {}
--- tic-tac-toe/src/TicTocGrid.js
+++ tic-tac-toe/src/TicTocGrid.js
@@ -1,25 +1,30 @@
-// import { useState } from "react";
-
+import { useState } from "react";
+// import Game from './Game'
 export default function TicTocGrid() {
-  //PSYDOCODE*************
-  //const player1 = x
-  //const player2 = o
+  const [player, setPlayer] = useState(["X"]);
 
-  //  \/\/\/\/\/\/\/\/\/
-  //  NEXT TIME:
-  //     FIGURE OUT A WAY TO PUT THE X FIRST THEN O(BACK AND FORTH). ON EACH SPACE PER TURN
-  //     OR PER CLICK. THE TOGGLING WORKED BUT THE MAYBE MAKE IT A LITTLE BIT MORE SIMPLIER MIND SET
-  //     NEXT TIME. LIKE, MAKE SURE IT GOES TO THE CORRECT SPACES FIRST. THEN
-  // const [x, setX] = useState("X");
-  // const [o, setO] = useState("O");
-  // const [clickedSpace, setClickedSpace] = useState();
+  let lastItemNum = player.length - 1;
+
+  function changeXorO() {
+    if (player[lastItemNum] === "X") {
+      console.log(lastItemNum);
+      setPlayer((player) => [...player, "O"]);
+      const playerX = <p>{player.at(-2)}</p>;
+      console.log(player);
+      return <div>{playerX}</div>;
+    } else {
+      console.log(lastItemNum);
+      const playerO = <p>{player.at(-2)}</p>;
+      setPlayer((player) => [...player, "X"]);
+      console.log(player);
+      return playerO;
+    }
+  }
 
   return (
     <>
       <div className="topRow">
-        <div className="topLeft space">
-          {/* <option className="clickXorO" value={fillInValue}></option> */}
-        </div>
+        <div className="topLeft space" onClick={changeXorO}></div>
         <div className="topCenter space"></div>
         <div className="topRight space"></div>
       </div>
