aab1393b4ed1874e5a2f226e8c556df8b4aea340
Andrew Aramouni <aramouni8@gmail.com>
2023-01-05 18:52:44 -0500

    removing some files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import "./TicTocGrid.css";
 import TicTocGrid from "./TicTocGrid.js";
 
---
+++ src/TicTocGrid.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+// import Game from './Game'
+export default function TicTocGrid() {
+  return (
+    <>
+      <div className="topRow">
+        <div className="topLeft space"></div>
+        <div className="topCenter space"></div>
+        <div className="topRight space"></div>
+      </div>
+      <div className="middleRow">
+        <div className="middleLeft space"></div>
+        <div className="middleCenter space"></div>
+        <div className="middleRight space"></div>
+      </div>
+      <div className="bottomRow">
+        <div className="bottomLeft space"></div>
+        <div className="bottomMiddle space"></div>
+        <div className="bottomRight space"></div>
+      </div>
+    </>
+  );
+}
